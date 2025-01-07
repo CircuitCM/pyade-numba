63502249f164ae90914e621f153efcf812a8d380
Charles Marks <charlesmarksco@gmail.com>
2025-01-07 12:50:48 -0600

    Flit edit

--- gopt/__init__.py
+++ gopt/__init__.py
@@ -1,3 +1,7 @@
+
+#Docy Stringy UwwWU
+"""Pyade with end-to-end njit implementations."""
+
 __version__ = '1.2' #One version ahead of the pypi version, also includes fixed population init from original.
 
 __all__ = ["de", "jade", "sade", "shade", "lshade", "ilshade", "lshadecnepsin", "saepsdemmts", "commons", "mpede",
